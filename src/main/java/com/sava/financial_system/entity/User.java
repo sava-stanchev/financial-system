a37461037d6e7ca2f5b5a30392ca0c5f229bcaba
sava-stanchev <s.stanchev94@gmail.com>
2026-05-09 15:26:35 +0300

    add UserController and fix User entity mapping

--- src/main/java/com/sava/financial_system/entity/User.java
+++ src/main/java/com/sava/financial_system/entity/User.java
@@ -19,4 +19,43 @@ public class User {
     private String firstName;
 
     private String lastName;
+
+    public User() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
