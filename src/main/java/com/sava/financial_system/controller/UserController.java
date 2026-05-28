18d4d006f6e1cda62a196d7e1d6632a9d61a1cf2
sava-stanchev <s.stanchev94@gmail.com>
2026-05-28 21:31:08 +0300

    user controller

---
+++ src/main/java/com/sava/financial_system/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.sava.financial_system.controller;
+
+import com.sava.financial_system.entity.User;
+import com.sava.financial_system.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
