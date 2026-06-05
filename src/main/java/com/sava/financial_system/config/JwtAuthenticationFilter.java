8a969b39df09c91a79318a454e8cf26941729873
sava-stanchev <s.stanchev94@gmail.com>
2026-06-05 20:28:00 +0300

    jwt auth filter

--- src/main/java/com/sava/financial_system/config/JwtAuthenticationFilter.java
+++ src/main/java/com/sava/financial_system/config/JwtAuthenticationFilter.java
@@ -1,4 +1,56 @@
 package com.sava.financial_system.config;
 
-public class JwtAuthenticationFilter {
+import com.sava.financial_system.service.JwtTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String tok = extractTokenFromHeader(req);
+
+            if (tok != null && jwtTokenProvider.validateToken(tok)) {
+                String userId = jwtTokenProvider.getUserIdFromToken(tok).toString();
+                String role = jwtTokenProvider.getRoleFromToken(tok);
+                String email = jwtTokenProvider.getEmailFromToken(tok);
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        email,
+                        null,
+                        Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role))
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception ex) {
+            System.err.println("Cannot set user authentication: " + ex.getMessage());
+        }
+
+        filterChain.doFilter(req, res);
+    }
+
+    private String extractTokenFromHeader(HttpServletRequest req) {
+        String header = req.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer "))
+            return header.substring(7);
+
+        return null;
+    }
 }
