a714ca7c919f9cadcbabf6a50b1e2d5348761dab
sava-stanchev <s.stanchev94@gmail.com>
2026-06-05 20:36:03 +0300

    update security config

--- src/main/java/com/sava/financial_system/config/SecurityConfiguration.java
+++ src/main/java/com/sava/financial_system/config/SecurityConfiguration.java
@@ -1,32 +1,42 @@
 package com.sava.financial_system.config;
 
+import com.sava.financial_system.service.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfiguration(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/api/health").permitAll()
                         .requestMatchers("/users/**").permitAll()
-                        .requestMatchers("/accounts/**").permitAll()
-                        .requestMatchers("/balances/**").permitAll()
-                        .requestMatchers("/transactions/**").permitAll()
-                        .requestMatchers("/transfers/**").permitAll()
+                        .requestMatchers("/accounts/**").authenticated()
+                        .requestMatchers("/balances/**").authenticated()
+                        .requestMatchers("/transactions/**").authenticated()
+                        .requestMatchers("/transfers/**").authenticated()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
